70516d9cf844570586111f4d72f3a93fd125f3cf
Arjun Savel <35353555+arjunsavel@users.noreply.github.com>
2021-02-22 08:37:25 -0800

    Add more photdata tests

--- tests/test_photdata.py
+++ tests/test_photdata.py
@@ -1,14 +1,96 @@
 import unittest
+import numpy as np
 from PIPS import photdata
 
 class TestPhotdata(unittest.TestCase):
+    data = [[1,2,3], [4,5,6], [7,8,9]]
 
     def test_photdata_initialization(self):
         try:
-            data = [1,2,3]
-            object = photdata(data)
+            object = photdata(self.data)
             instantiated = True
         except Exception as e:
             print(e)
             instantiated = False
         self.assertTrue(instantiated)
+        
+    def test_prepare_data_pass_nones(self):
+        object = photdata(self.data)
+        try:
+            x, y, yerr = object.prepare_data(None, None, None)
+            prepared = True
+        except Exception as e:
+            print(e)
+            prepared = False
+        self.assertTrue(prepared)
+        
+        
+    def test_prepare_data_pass_vals(self):
+        object = photdata(self.data)
+        try:
+            x, y, yerr = object.prepare_data(
+                                            self.data[0],
+                                            self.data[1],
+                                            self.data[2])
+            prepared = True
+        except Exception as e:
+            print(e)
+            prepared = False
+        self.assertTrue(prepared)
+        
+        
+    def test_prepare_data_incomplete(self):
+        object = photdata(self.data)
+        try:
+            x, y, yerr = object.prepare_data(
+                                            self.data[0],
+                                            self.data[1],
+                                            None)
+            prepared = True
+        except Exception as e:
+            print(e)
+            prepared = False
+        self.assertFalse(prepared)
+        
+    def test_cut_xmin(self):
+        object = photdata(self.data)
+        object.cut(xmin=2)
+        self.assertTrue(np.all(photdata.x >= 2))
+    
+    def test_cut_x_max(self):
+        object = photdata(self.data)
+        object.cut(xmax=2)
+        self.assertTrue(np.all(photdata.x <= 2))
+        
+    def test_cut_ymin(self):
+        object = photdata(self.data)
+        object.cut(ymin=5)
+        self.assertTrue(np.all(photdata.y >= 5))
+        
+    def test_cut_ymax(self):
+        object = photdata(self.data)
+        object.cut(ymax=5)
+        self.assertTrue(np.all(photdata.y <= 5))
+        
+    def test_cut_yerrmin(self):
+        object = photdata(self.data)
+        object.cut(yerr_min=8)
+        self.assertTrue(np.all(photdata.y >= 8))
+        
+    def test_cut_yerrmax(self):
+        object = photdata(self.data)
+        object.cut(yerr_max=8)
+        self.assertTrue(np.all(photdata.y <= 8))
+        
+                        
+    
+                        
+                        
+                        
+                    
+                        
+                        
+        
+    
+            
+        
