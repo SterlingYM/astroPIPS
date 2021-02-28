eaf165b504195f4167dc09a001d81ec9c9d1ba4d
Arjun Savel <asavel@berkeley.edu>
2021-02-28 17:59:41 -0500

    Finish testing init of stellar models

--- tests/test_stellar_models.py
+++ tests/test_stellar_models.py
@@ -9,10 +9,51 @@ class TestStellarInit(unittest.TestCase):
         not_photdata = [] # empty list that isn't a photdata object
         obj = PIPS.StellarModels(not_photdata)
         try:
-            obj.C_k
+            obj.C_1
             correctly_instantiated = False
         except AttributeError:
             # the object shouldn't have instantiated because it's not a photdata object!
             correctly_instantiated = True 
             
         self.assertTrue(correctly_instantiated)
+        
+        
+        
+    def test_init_not_run_ger_period(self):
+        x = np.linspace(0, 100, 1000)
+        y = np.sin(x/2)
+        yerr = np.ones_like(y) * .01
+
+        star = PIPS.photdata([x, y, yerr])
+        obj = PIPS.StellarModels(star)
+        try:
+            obj.C_1
+            correctly_instantiated = False
+        except AttributeError:
+            # the object shouldn't have instantiated because get_period hasn't been run!
+            correctly_instantiated = True 
+            
+        self.assertTrue(correctly_instantiated)
+        
+        
+    def test_init_normal(self):
+        x = np.linspace(0, 100, 1000)
+        y = np.sin(x/2)
+        yerr = np.ones_like(y) * .01
+
+        star = PIPS.photdata([x, y, yerr])
+        star.get_period()
+        star.get_epoch_offset()
+        obj = PIPS.StellarModels(star)
+        try:
+            obj.C_1
+            correctly_instantiated = True
+        except AttributeError:
+            # the object shouldn't False instantiated because get_period hasn't been run!
+            correctly_instantiated = True 
+            
+        self.assertTrue(correctly_instantiated)
+        
+        
+        
+        
