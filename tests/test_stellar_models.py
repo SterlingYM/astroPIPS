136a972f7d3871d2332ab33f7350782286fa31bd
Arjun Savel <asavel@berkeley.edu>
2021-02-28 18:14:25 -0500

    Add quick Cacciari2005 test

--- tests/test_stellar_models.py
+++ tests/test_stellar_models.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 import PIPS
+import io
 
 
 class TestStellarInit(unittest.TestCase):
@@ -54,6 +55,26 @@ class TestStellarInit(unittest.TestCase):
             
         self.assertTrue(correctly_instantiated)
         
+        
+class TestCacciari2005(unittest.TestCase):
+    
+    def test_capture_correct_stellar_type(self):
+        correct_string = """Not a valid input. star_type must be a string of the form 'RRab' or 'RRc' in order to work"""
+        x = np.linspace(0, 100, 1000)
+        y = np.sin(x/2)
+        yerr = np.ones_like(y) * .01
+
+        star = PIPS.photdata([x, y, yerr])
+        star.get_period()
+        star.get_epoch_offset(model='Fourier', N_peak_test=1000, p_min=0.1,p_max=20)
+        
+        obj  = PIPS.Cacciari2005(star)
+        print_output = io.StringIO()                  
+        sys.stdout = capturedOutput                     
+        obj.calc_all_vals('')                                     
+        sys.stdout = sys.__stdout__                     
+
+        self.assertEqual(print_output.getvalue(), correct_string)
         
         
         
