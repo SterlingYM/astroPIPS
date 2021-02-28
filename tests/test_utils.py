da1684a515967fa8477cd075d975b7b21510b9d4
Arjun Savel <asavel@berkeley.edu>
2021-02-28 14:10:49 -0500

    Add utils testing

---
+++ tests/test_utils.py
@@ -0,0 +1,45 @@
+import unittest
+import numpy as np
+from PIPS import photdata
+
+class TestLPP(unittest.TestCase):
+    
+    def test_data_readin_LPP_regression_x(self):
+        """
+        Regression test against previously read-in 
+        LPP data.
+        """
+        data = PIPS.data_readin_LPP('sample_data/005.dat',filter='V')
+        x,y,yerr = data
+        
+        # read in the previous data
+        x_test = np.loadtxt('tests/test_lpp_x.txt')
+        np.testing.assert_array_equal(x, x_test)
+        
+        
+    def test_data_readin_LPP_regression_y(self):
+        """
+        Regression test against previously read-in 
+        LPP data.
+        """
+        data = PIPS.data_readin_LPP('sample_data/005.dat',filter='V')
+        x,y,yerr = data
+        
+        # read in the previous data
+        y_test = np.loadtxt('tests/test_lpp_y.txt')
+        np.testing.assert_array_equal(y, y_test)
+        
+        
+    def test_data_readin_LPP_regression_yerr(self):
+        """
+        Regression test against previously read-in 
+        LPP data.
+        """
+        data = PIPS.data_readin_LPP('sample_data/005.dat',filter='V')
+        x,y,yerr = data
+        
+        # read in the previous data
+        yerr_test = np.loadtxt('tests/test_lpp_yerr.txt')
+        np.testing.assert_array_equal(yerr, yerr_test)
+        
+        
